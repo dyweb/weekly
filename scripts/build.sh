8bed594475d03eeb17a09c1875573d1969626391
Ce Gao <gaoce@caicloud.io>
2018-05-03 17:09:02 +0800

    build: Move to script
    Signed-off-by: Ce Gao <gaoce@caicloud.io>

---
+++ scripts/build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+ROOT=$(dirname ${BASH_SOURCE})/..
+ROOT=$(realpath ${ROOT})
+
+WORKDIR=${ROOT}/.jekyll
+POSTDIR=${WORKDIR}/_posts
+
+echo 'root = '${ROOT}
+echo 'workdir ='${WORKDIR}
+
+cd ${WORKDIR}
+mkdir -p ${POSTDIR}
+seq 2015 `date +%Y` | xargs -I% mv ${ROOT}/% ${POSTDIR}
+
+bundle exec jekyll build --source ${WORKDIR} --destination ${ROOT}/docs
+ls ${POSTDIR} | xargs -I% mv ${POSTDIR}/% ${ROOT}
+cd - > /dev/null
